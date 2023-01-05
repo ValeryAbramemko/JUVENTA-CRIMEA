8b8cebfc13919fe5dbd9eb21e9f62b15badfe756
AbramenkoValery <vvabramenko@vk.com>
2023-01-05 22:11:27 +0300

    three

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
@@ -42,8 +41,6 @@ btnMenu.addEventListener("click", function () {
     }
 });
 
-
-
 btns = document.querySelector(".row").querySelectorAll(".column");
 btns.forEach(element => {
     element.addEventListener("click", function () {
@@ -53,21 +50,40 @@ btns.forEach(element => {
     });
 });
 
-
-
-
-const btnFav = document.querySelector('[data-action="favourites"]');
+const btnBaskets = document.querySelector('[data-action="baskets"]');
+const btnFavourites = document.querySelector('[data-action="favourites"]');
+const caunterLike = document.querySelector('[data-action="like"]');
 const caunter = document.querySelector('[data-caunter]');
 const total = document.querySelector('[data-total]');
 const price = document.querySelector('[data-price]');
+const caunterCompare = document.querySelector('[data-action="compare"]');
+const btnCompare = document.querySelector('[data-action="btncompare"]');
 
-btnFav.addEventListener('click', function () {
+btnCompare.addEventListener('click', function () {
+    if (caunterCompare.innerText <= 0) {
+        caunterCompare.innerText = 1;
+        btnCompare.innerText = 'Удалить'
+    } else {
+        caunterCompare.innerText = 0;
+        btnCompare.innerText = 'Сравнить';
+    };
+});
 
-    caunter.innerText = ++caunter.innerText;
-    total.innerText = parseInt(total.innerText) + parseInt(price.innerText);
-
+btnFavourites.addEventListener('click', function () {
+    if (caunterLike.innerText <= 0) {
+        caunterLike.innerText = 1;
+        btnFavourites.innerText = 'Убрать';
+    } else {
+        caunterLike.innerText = 0;
+        btnFavourites.innerText = 'В ИЗБРАННОЕ';
+    }
 
 })
+
+btnBaskets.addEventListener('click', function () {
+    caunter.innerText = ++caunter.innerText;
+    total.innerText = parseInt(total.innerText) + parseInt(price.innerText);
+});
 
 
 
